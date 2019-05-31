d4787b57fc6ff5c53ea4d0fc8cc9096d1753eaa7
ViktorKch <43659721+ViktorKch@users.noreply.github.com>
2019-06-01 00:26:33 +0700

    Create value.js

---
+++ value.js
@@ -0,0 +1,9 @@
+// let myMap = new Map().set('a', 1).set('b', 2);
+
+// console.log(myMap)
+
+// let keys =[ ...myMap.keys() ];
+// let values =[ ...myMap.values() ]
+
+// console.log(keys)
+// console.log(values)
